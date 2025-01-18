b141129cc8cc51b3cb07f21f4a000eb719d5f688
adriacabeza <adria.cabezasantanna@datadoghq.com>
2025-01-18 16:53:09 +0100

    Add test module

--- src/clustering/utils.rs
+++ src/clustering/utils.rs
@@ -1,5 +1,5 @@
-use ndarray::{array, Array1, ArrayView2, Axis};
-use num_traits::{FromPrimitive, ToPrimitive};
+use ndarray::{Array1, ArrayView2, Axis};
+use num_traits::FromPrimitive;
 use crate::core::float::AdriannFloat;
 
 pub fn compute_mean<F>(data: &ArrayView2<F>, indices: &[usize]) -> Array1<F>
@@ -14,14 +14,19 @@ where
     selected_data.mean_axis(Axis(0)).unwrap()
 }
 
-#[test]
-fn test_compute_mean() {
-    let data = array![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
-    let indices = vec![0, 2]; // Select the first and third rows
+#[cfg(test)]
+mod tests {
+    use num_traits::ToPrimitive;
+    use ndarray::array;
+    #[test]
+    fn test_compute_mean() {
+        let data = array![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
+        let indices = vec![0, 2]; // Select the first and third rows
 
-    let result = compute_mean(&data.view(), &indices);
-    let expected = array![3.0, 4.0]; // Mean of [1.0, 2.0] and [5.0, 6.0]
+        let result = compute_mean(&data.view(), &indices);
+        let expected = array![3.0, 4.0]; // Mean of [1.0, 2.0] and [5.0, 6.0]
 
-    assert!((result[0] - expected[0]).to_f64().unwrap().abs() < 1e-6, "Expected {}, got {}", expected[0], result[0]);
-    assert!((result[1] - expected[1]).to_f64().unwrap().abs() < 1e-6, "Expected {}, got {}", expected[1], result[1]);
+        assert!((result[0] - expected[0]).to_f64().unwrap().abs() < 1e-6, "Expected {}, got {}", expected[0], result[0]);
+        assert!((result[1] - expected[1]).to_f64().unwrap().abs() < 1e-6, "Expected {}, got {}", expected[1], result[1]);
+    }
 }
