1f27f08ba813597d5c44bd308b1097d772998833
adriacabeza <adria.cabezasantanna@datadoghq.com>
2025-01-18 17:14:55 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@
 /// This library is based on SPANN, a highly-efficient billion-scale approximate nearest neighbor search.
 ///
 /// # Modules
-/// - `clustering`: Contains the hierarchical clustering implementation.
+/// - `clustering`: Contains the implementation of the hierarchical clustering algorithm used in SPANN.
+/// - `core`: Contains the core float data type.
+/// - `distances`: Contains the implementation of the distance metrics used everywhere in the project.
+/// - `spann`: Contains the implementation of the SPANN index.
 pub mod clustering;
 pub mod core;
 pub mod distances;
